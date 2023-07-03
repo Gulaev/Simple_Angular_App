fb7938ee0434d40b4ae1a738b53f9929c6a36b5d
Denis Gulaev <dennysittohero@gmail.com>
2023-07-03 22:22:29 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/product'
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'selection_room';
+  title = 'Hello Shop'
+  products : IProduct[] = data
+  
 }
